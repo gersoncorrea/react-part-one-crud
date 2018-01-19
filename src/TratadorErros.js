2cce3651fc0f33fc7a66a5902783ce2590d14bd9
anonimo <anonimo@gmail.com>
2018-01-19 08:01:12 -0400

    bug#pubsub
    -- erro de digitacao

--- src/TratadorErros.js
+++ src/TratadorErros.js
@@ -4,7 +4,7 @@ export default class TratadorErros{
     publicErros(erros){
        for(var i=0;i<erros.errors.lenght;i++){
             var erro = erros.errors[i];
-            PubSub.publis("erro-validacao",erro);
+            PubSub.publish("erro-validacao",erro);
         }
     }
 }
